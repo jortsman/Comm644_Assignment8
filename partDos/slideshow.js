ee1f279f87c1d44a7265b3ce0f45e9e3aca70776
Vlad Pasculescu <vpasculescu@Vlads-MacBook-Pro.local>
2017-04-20 18:01:41 -0700

    part2 update

--- partDos/slideshow.js
+++ partDos/slideshow.js
@@ -14,4 +14,28 @@ $(function() {
   $('#counter p:last-of-type').on('click', function() {
     $('#slider').css("background-image", "url('img/5.jpg')");
   });
+  $('#tabs ul li:first-child').on('click', function() {
+    $('#tabs ul li:first-child').addClass('active');
+    $('#tabs ul li:nth-child(2)').removeClass('active');
+    $('#tabs ul li:nth-child(3)').removeClass('active');
+    $('#panel1').removeClass('hide');
+    $('#panel2').addClass('hide');
+    $('#panel3').addClass('hide');
+  });
+  $('#tabs ul li:nth-child(2)').on('click', function() {
+    $('#tabs ul li:nth-child(1)').removeClass('active');
+    $('#tabs ul li:nth-child(2)').addClass('active');
+    $('#tabs ul li:nth-child(3)').removeClass('active');
+    $('#panel1').addClass('hide');
+    $('#panel2').removeClass('hide');
+    $('#panel3').addClass('hide');
+  });
+  $('#tabs ul li:nth-child(3)').on('click', function() {
+    $('#tabs ul li:nth-child(1)').removeClass('active');
+    $('#tabs ul li:nth-child(2)').removeClass('active');
+    $('#tabs ul li:nth-child(3)').addClass('active');
+    $('#panel1').addClass('hide');
+    $('#panel2').addClass('hide');
+    $('#panel3').removeClass('hide');
+  });
 });
